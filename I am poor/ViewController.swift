50be5bad97a55fa3d6644f659660c19987155917
John Astrolabio <Johna32@learn.cssd.ab.ca>
2019-02-12 12:42:50 -0700

    Ver #1

---
+++ I am poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by John Astrolabio on 2019-02-08.
+//  Copyright © 2019 John Astrolabio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
